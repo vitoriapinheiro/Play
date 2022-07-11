d1e5ed8608eb1777d52c09418b4ffc07f36850cc
vitoriapinheiro <vps3@cin.ufpe.br>
2022-07-11 14:07:48 -0300

    Recreated the project

---
+++ Play/Play/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Play
+//
+//  Created by vivi on 07/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ToWatch/ToWatch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToWatch
+//
+//  Created by vivi on 11/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
